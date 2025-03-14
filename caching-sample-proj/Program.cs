a37b2fa76932f55b1ca62c9d3bf17ef58f621b93
Abiraj UCA <abiraj@unicornconnectedapps.com>
2025-03-14 10:04:33 +0530

    Add project files.

---
+++ caching-sample-proj/Program.cs
@@ -0,0 +1,49 @@
+
+namespace caching_sample_proj
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+
+            // Add Memory Cache
+            builder.Services.AddMemoryCache();
+
+            // Add Distributed Cache
+            builder.Services.AddDistributedMemoryCache();
+
+            // In a real app, you might use Redis or SQL Server cache:
+            builder.Services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = builder.Configuration.GetConnectionString("Redis");
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
